3fbeb8c5b8ba08c7df7ff1ed0612e5d872437377
sunday-sunny <22mallox@gmail.com>
2020-05-05 22:14:59 +0900

    add props example

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React from "react";
 
+const FruitList = [
+  { id: 1, name: "Peach", emoji: "🍑" },
+  { id: 2, name: "Orange", emoji: "🍊" },
+  { id: 3, name: "Kiwi", emoji: "🥝" },
+  { id: 4, name: "Banana", emoji: "🍌" },
+];
+
+function Fruit({ name, emoji }) {
+  return (
+    <div>
+      <h3>
+        I love {name} {emoji}
+      </h3>
+    </div>
+  );
+}
+
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
+      {FruitList.map((piece) => (
+        <Fruit key={piece.id} name={piece.name} emoji={piece.emoji} />
+      ))}
     </div>
   );
 }
